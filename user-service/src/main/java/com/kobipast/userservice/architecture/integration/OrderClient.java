6abab3d837e5335a0aaea017e805eab0ad3663c9
kobipast <kobi_h@hotmail.com>
2026-01-10 20:58:59 +0200

    add GET and PATCH request to order ms

--- user-service/src/main/java/com/kobipast/userservice/architecture/integration/OrderClient.java
+++ user-service/src/main/java/com/kobipast/userservice/architecture/integration/OrderClient.java
@@ -2,6 +2,7 @@ package com.kobipast.userservice.architecture.integration;
 
 import com.kobipast.userservice.architecture.integration.dto.CreateOrderRequest;
 import com.kobipast.userservice.architecture.integration.dto.OrderDto;
+import com.kobipast.userservice.architecture.integration.dto.UpdateOrderStatusRequest;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Service;
@@ -39,4 +40,34 @@ public class OrderClient {
                 -1
         );
     }
+
+    @CircuitBreaker(name = "orders", fallbackMethod = "getFallback")
+    @Retry(name = "orders")
+    public OrderDto getOrder(String orderId) {
+        return webClient.get()
+                .uri("/orders/{id}", orderId)
+                .retrieve()
+                .bodyToMono(OrderDto.class)
+                .block();
+    }
+
+    private OrderDto getFallback(String orderId, Throwable t) {
+        return new OrderDto(orderId, "fallback", "FAILED", 0, Instant.now().toString(), -1);
+    }
+
+    @CircuitBreaker(name = "orders", fallbackMethod = "updateFallback")
+    @Retry(name = "orders")
+    public OrderDto updateStatus(String orderId, UpdateOrderStatusRequest req) {
+        return webClient.patch()
+                .uri("/orders/{id}/status", orderId)
+                .bodyValue(req)
+                .retrieve()
+                .bodyToMono(OrderDto.class)
+                .block();
+    }
+
+    private OrderDto updateFallback(String orderId, UpdateOrderStatusRequest req, Throwable t) {
+        return new OrderDto(orderId, "fallback", "FAILED", 0, Instant.now().toString(), -1);
+    }
+
 }
